c068acf1f76e9cda3cee17d1bb25dec5f5cada06
Jimmy <>
2021-04-17 11:09:01 -0400

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -12,17 +12,21 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
+
+const chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("assets/data/data.csv").then(data => {
   data.forEach(d => {
     d.id = +d.id;
+    d.state == d.state;
+    d.abbr == d.abbr;
     d.poverty = +d.poverty;
     d.povertyMoe = +d.povertyMoe;
     d.age = +d.age;
@@ -38,6 +42,9 @@ d3.csv("assets/data/data.csv").then(data => {
     d.smokes = +d.smokes;
     d.smokesHigh = +d.smokesHigh;
     d.smokesLow = +d.smokesLow;
+    abc = [];
+    abc.push(d.abbr);
+    console.log(abc)
   });
 
   var x = d3.scaleLinear()
@@ -47,7 +54,7 @@ d3.csv("assets/data/data.csv").then(data => {
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x));
   var y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.healthcare)])
+    .domain([0, d3.max(data, d => d.obesity)])
     .range([height, 0]);
   chartGroup.append("g")
     .call(d3.axisLeft(y));
@@ -58,25 +65,74 @@ d3.csv("assets/data/data.csv").then(data => {
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d.poverty); } )
-      .attr("cy", function (d) { return y(d.healthcare); } )
-      .attr("r", 8)
+      .attr("cx", d => x(d.poverty))
+      .attr("cy", d => y(d.obesity))
+      .attr("r", "15")
       .style("fill", "#69b3a2")
+      .attr("opacity", ".5");
 
-    //   x.domain([1000, 0])
-    // svg.select(".myXaxis")
-    //   .transition()
-    //   .duration(2000)
-    //   .attr("opacity", "1")
-    //   .call(d3.axisBottom(x));
-    //
-    // svg.selectAll("circle")
-    //   .transition()
-    //   .delay(function(d,i){return(i*3)})
-    //   .duration(2000)
-    //   .attr("cx", function (d) { return x(d.poverty); } )
-    //   .attr("cy", function (d) { return y(d.healthcare); } )
-})
+  chartGroup.selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(function(d){
+      return d.abbr; })
+    .attr("x", d => x(d.poverty) - 8)
+    .attr("y", d => y(d.obesity) + 4)
+    .attr("font_family", "sans-serif")  // Font type
+    .attr("font-size", "11px")  // Font size
+    .attr("fill", "black");   // Font color
 
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(d => (`State: ${d.state}`));
+
+  chartGroup.call(toolTip);
+
+  circleGroup
+    .on("click", data => toolTip.show(data, this))
+    .on("mouseout", (d, i) => toolTip.hide(d));
+
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 10)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Poverty");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Obesity");
+
+    grid = g => g
+        .attr("stroke", "currentColor")
+        .attr("stroke-opacity", 0.1)
+        .call(g => g.append("g")
+          .selectAll("line")
+          .data(x.ticks())
+          .join("line")
+            .attr("x1", d => 0.5 + x(d))
+            .attr("x2", d => 0.5 + x(d))
+            .attr("y1", margin.top)
+            .attr("y2", height - margin.bottom))
+        .call(g => g.append("g")
+          .selectAll("line")
+          .data(y.ticks())
+          .join("line")
+            .attr("y1", d => 0.5 + y(d))
+            .attr("y2", d => 0.5 + y(d))
+            .attr("x1", margin.left)
+            .attr("x2", width - margin.right));
+
+            svg.append("g")
+              .call("grid");
+}).catch(function(error) {
+    console.log(error);
+
+});
 
 // console.log(sourceData[0])
